46aec78218b7fc9fe76301192a778bb6d6fbc72a
ilyawebdevelop <ilia966@tut.by>
2024-04-25 10:39:59 +0300

    start

---
+++ src/js/app.js
@@ -0,0 +1,101 @@
+import * as flsFunctions from "./modules/functions.js";
+import "./modules/jquery-3.7.1.min.js";
+
+import { Fancybox } from "./modules/fancybox.esm.js";
+
+import "./modules/bootstrap.bundle.min.js";
+import './components.js';
+
+flsFunctions.isWebp();
+
+Fancybox.bind("[data-fancybox]", {
+  closeButton: false,
+});
+
+// Import swiper
+import Swiper, { Navigation, Pagination, Autoplay, Mousewheel, EffectFade, Thumbs, Scrollbar } from 'swiper';
+Swiper.use([Navigation, Pagination, Autoplay, Mousewheel, EffectFade, Thumbs, Scrollbar]);
+
+// contentSlider
+const contentSlider = document.querySelector('.contentSlider');
+var mySwiperContent = new Swiper(contentSlider, {
+  slidesPerView: 2,
+  speed: 800,
+  spaceBetween: 23,
+  autoplay: {
+    delay: 3000,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 2,
+    },   
+  },
+  navigation: {
+    prevEl: document.querySelector('.contentSliderW .navArrowPrev'),
+    nextEl: document.querySelector('.contentSliderW .navArrowNext'),
+  },
+});
+
+// Burger
+const btnMenu = document.querySelector('#toggle');
+const menu = document.querySelector('.header .headerNav');
+const bodyEl = document.querySelector('body');
+const btnClose = document.querySelector('.headerNavCloseBtn');
+const overlay = document.querySelector('.overlay');
+
+const toggleMenu = function () {
+  menu.classList.toggle('active');
+}
+const toggleBurger = function () {
+  btnMenu.classList.toggle('active');
+}
+const bodyOverflow = function () {
+  bodyEl.classList.toggle('hidden');
+}
+const menuClose = function () {
+  toggleBurger();
+  bodyOverflow();
+  toggleMenu();
+}
+
+btnMenu?.addEventListener('click', function (e) {
+  e.stopPropagation();
+  toggleMenu();
+  toggleBurger();
+  bodyOverflow();
+  overlay.classList.add('active');
+});
+
+btnClose?.addEventListener('click', function (e) {
+  menuClose();
+  overlay.classList.remove('active');
+});
+
+let headerSearchBtn = document.querySelector('.headerSearchBtn');
+let headerSearch = document.querySelector('.headerSearch');
+let headerSearchClose = document.querySelector('.headerSearchClose');
+headerSearchBtn.addEventListener('click', () => {
+  headerSearch.classList.toggle('active');
+});
+headerSearchClose.addEventListener('click', () => {
+  headerSearch.classList.toggle('active');
+});
+
+// filter show portal page
+let filterBtn = document.querySelector('.filterBtn');
+let filterPortal = document.querySelector('.filterPortal');
+filterBtn?.addEventListener('click', () => {
+  filterPortal.classList.toggle('active');
+});
+
+overlay?.addEventListener('click', (e) => {
+  e.stopPropagation();
+  overlay.classList.remove('active');
+  menu.classList.remove('active');
+  btnMenu.classList.remove('active');
+  bodyEl.classList.remove('hidden');
+});
